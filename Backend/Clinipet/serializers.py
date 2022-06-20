a827cba51b63f45b1b631c59cde93abb772148fe
AtachijoyR <yerko.pizarro@alumnos.ucn.cl>
2022-06-20 01:31:43 -0400

    feat: Pruebas de manejo de parametros.

--- Backend/Clinipet/serializers.py
+++ Backend/Clinipet/serializers.py
@@ -7,3 +7,13 @@ class PetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pet
         fields =('__all__') 
+
+class PetSerializerWithDescription(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    breed = serializers.CharField()
+    age_months = serializers.IntegerField()
+    weight = serializers.IntegerField()
+    status = serializers.CharField()
+    specie = serializers.CharField()
+    rut_owner = serializers.CharField()
